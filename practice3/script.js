bab640099cc37de750a78015e4e18bd07585fb81
divergen371 <divergen@protonmail.com>
2021-08-25 00:40:29 +0900

    完成

--- practice3/script.js
+++ practice3/script.js
@@ -1,9 +1,42 @@
 let osDark = window.matchMedia("(prefers-color-scheme: dark)");
+let displayColorSchemeSwitch = document.getElementById("myonoffswitch");
 
 function darkModeOn() {
     document.documentElement.classList.add("darkmode");
+    displayColorSchemeSwitch.checked = true;
+}
+
+function darkModeOff() {
+    document.documentElement.classList.remove("darkmode");
+    displayColorSchemeSwitch.checked = false;
 }
 
 if (osDark.matches) {
     darkModeOn();
 }
+osDark.addListener(function () {
+    if (osDark.matches) {
+        darkModeOn();
+    } else {
+        darkModeOff();
+    }
+})
+
+if (sessionStorage.getItem("darkMode") === "on") {
+    darkModeOn();
+} else if (sessionStorage.getItem("darkmode") === "off") {
+    darkModeOff();
+} else if (osDark.matches) {
+    darkModeOn();
+}
+
+displayColorSchemeSwitch.addEventListener("change", function () {
+    if (displayColorSchemeSwitch.checked) {
+        darkModeOn();
+        sessionStorage.setItem("darkMode","on");
+    } else {
+        darkModeOff();
+        sessionStorage.setItem("darkMode","off");
+    }
+})
+
